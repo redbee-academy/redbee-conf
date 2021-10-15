405903e570455cc25749521916719f6d30e3c8a7
Joselina <joselina.ramirez@redb.ee>
2021-10-15 16:17:43 -0300

    feat:TalkDao complete, model complete

--- ms-talks/src/main/java/io/redbee/product/conf/ms/talks/talks/dao/TalkDao.java
+++ ms-talks/src/main/java/io/redbee/product/conf/ms/talks/talks/dao/TalkDao.java
@@ -1,15 +1,17 @@
 package io.redbee.product.conf.ms.talks.talks.dao;
+import io.redbee.product.conf.ms.talks.talks.model.Talk;
 
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TalkDao<Talk> {
+public class TalkDao {
 
     private final NamedParameterJdbcTemplate template;
 
@@ -32,10 +34,29 @@ public class TalkDao<Talk> {
     private static final String insertQuery = "" + "INSERT INTO talks (redbee_employee, reference, talk_name, talk_topic, talk_description, creation_date)" +
             "VALUES (:redbee_employee, :reference, :talk_name, :talk_topic, :talk_description, :creation_date)";
 
-//    public void save (Talk talk){
-//        KeyHolder keyHolder = new GeneratedKeyHolder();
-//        template.update(insertQuery, keyHolder);
-//    }
+    public void save (Talk talk){
+        try {
+            template.update(insertQuery, talkToParamMap(talk));
+            LOGGER.info("save: talk {} saved", talk.getTalk_name());
+        } catch (Exception e) {
+            LOGGER.info("save: error {}, saving talk {}", e.getMessage(), talk.getTalk_name());
+            //throw new RepositoryException();
+        }
+
+    }
+
+    private MapSqlParameterSource talkToParamMap(Talk talk) {
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("id", talk.getId());
+        params.addValue("redbee_employee", talk.getRedbee_employee());
+        params.addValue("reference", talk.getReference());
+        params.addValue("talk_name", talk.getTalk_name());
+        params.addValue("talk_topic", talk.getTalk_topic());
+        params.addValue("talk_description", talk.getTalk_description());
+        params.addValue("creation_date", talk.getCreation_date());
+
+        return params;
+    }
 
 
 }
---
+++ ms-talks/src/main/java/io/redbee/product/conf/ms/talks/talks/model/Talk.java
@@ -0,0 +1,52 @@
+package io.redbee.product.conf.ms.talks.talks.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class Talk implements Serializable {
+    private final Integer id;
+    private final Boolean redbee_employee;
+    private final String reference;
+    private final String talk_name;
+    private final String talk_topic;
+    private final String talk_description;
+    private final LocalDateTime creation_date;
+
+    public Talk(Integer id, Boolean redbee_employee, String reference, String talk_name, String talk_topic, String talk_description, LocalDateTime creation_date) {
+        this.id = id;
+        this.redbee_employee = redbee_employee;
+        this.reference = reference;
+        this.talk_name = talk_name;
+        this.talk_topic = talk_topic;
+        this.talk_description = talk_description;
+        this.creation_date = creation_date;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Boolean getRedbee_employee() {
+        return redbee_employee;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public String getTalk_name() {
+        return talk_name;
+    }
+
+    public String getTalk_topic() {
+        return talk_topic;
+    }
+
+    public String getTalk_description() {
+        return talk_description;
+    }
+
+    public LocalDateTime getCreation_date() {
+        return creation_date;
+    }
+}
