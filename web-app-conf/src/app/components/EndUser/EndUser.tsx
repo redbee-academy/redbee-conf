dad6f351bd90101d12d664841d2546e018310ca3
RosaAlfonso1993 <rosa.alfonso1993@gmail.com>
2021-10-15 16:27:47 -0300

    feat: branch login

--- web-app-conf/src/app/components/EndUser/EndUser.tsx
+++ web-app-conf/src/app/components/EndUser/EndUser.tsx
@@ -1,8 +1,29 @@
 import { FunctionComponent } from "react";
 import { App } from "../App";
+import { GoogleLogin } from 'react-google-login';
 
-const EndUserApp: FunctionComponent = () => {
-  return <App><div>Edita <code>src/app/components/EndUser.tsx</code> para continuar</div></App>
+
+const responseGoogle = (response:any) => {
+  console.log(response);
 }
 
-export default EndUserApp
+const EndUserApp: FunctionComponent = () => {
+  return (
+    <App>
+      <div>
+        Edita <code>src/app/components/EndUser.tsx</code> para continuar
+      </div>
+      
+      <GoogleLogin
+        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
+      
+    </App>
+  );
+};
+
+export default EndUserApp;
