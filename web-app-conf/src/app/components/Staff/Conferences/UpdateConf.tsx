512fc1763439d4fe4d441a1b60cc39508a6aa556
Indiana Lozano <indilozano19@gmail.com>
2021-10-18 15:06:42 -0300

    feat(front): finalizo formulario estático

--- web-app-conf/src/app/components/Staff/Conferences/UpdateConf.tsx
+++ web-app-conf/src/app/components/Staff/Conferences/UpdateConf.tsx
@@ -1,12 +1,12 @@
 import { FunctionComponent } from "react";
-import { FloatingLabel, Form } from "react-bootstrap";
+import { Button, FloatingLabel, Form } from "react-bootstrap";
 
 const UpdateConf: FunctionComponent = () => {
   return <div  style={{ maxWidth: '600px', padding: '20px' }}>
       <h2>Editar Redbee Conf vol.5</h2>
   <Form>
   <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-    <Form.Label>Fecha Incio</Form.Label>
+    <Form.Label>Fecha Inicio</Form.Label>
     <Form.Control type="date" />
   </Form.Group>
   <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
@@ -14,16 +14,18 @@ const UpdateConf: FunctionComponent = () => {
     <Form.Control type="date" placeholder="name@example.com" />
   </Form.Group>
   <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Descripcion</Form.Label>
+        <Form.Label>Descripción</Form.Label>
         <Form.Control as="textarea" rows={3} />
     </Form.Group>
-    <FloatingLabel controlId="floatingSelect" label="Estado">
-    <Form.Select aria-label="Floating label select example">
+    <Form.Label>Estado</Form.Label>
+    <Form.Select>
         <option>Visible</option>
         <option value="1">Oculto</option>
     </Form.Select>
-    </FloatingLabel>
-   
+    <div style= {{display:"flex", justifyContent:"space-between", paddingTop: '20px'}} >
+      <Button variant="outline-danger">Cancelar</Button>{' '}
+      <Button variant="outline-primary">Guardar</Button>{' '}
+    </div>
     </Form>
   </div>
 
