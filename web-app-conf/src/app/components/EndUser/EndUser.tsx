95a8319cd600e8cea2dee7ffaa9ad7be40b5f049
LeonelaF <leonelafredes@gmail.com>
2021-10-21 00:28:14 -0300

    fix: se arregla Front para coincidir con landing UX

--- web-app-conf/src/app/components/EndUser/EndUser.tsx
+++ web-app-conf/src/app/components/EndUser/EndUser.tsx
@@ -1,33 +1,32 @@
-/* eslint-disable react/jsx-no-comment-textnodes */
 import { FunctionComponent } from "react";
-import { Button } from "react-bootstrap";
 import { App } from "../App";
-import { AppNavBar } from "../NavBar";
-import './EndUser.css'
-
+import './style.css'
+import img from './isologo_rbconf.png'
+import { Button } from "react-bootstrap";
 
 const EndUserApp: FunctionComponent = () => {
   return <App>
-  <AppNavBar/>
-	<body className="cuerpo" >
-  	<header>
-        <h1 className="title">redbeeConf-VOL5</h1>
-        <div  className="logotipo" >
-          <img className="imagen" src="https://media.discordapp.net/attachments/872157975920386071/899729747339247646/unknown.png" alt="Logo Redbee"/>
-        </div>
-    </header>
-    <div className="Mensajes">
-        <p> La próxima redbeeConf será el 28 de Marzo de 2022 a las 14:00</p>
-        <Button variant="light">Postulá tu charla!</Button>
-    </div>
-    <footer className="redes-sociales">
-      <a href="https://www.facebook.com/redbeestudios" className="btn-red-social"><i className="fab fa-facebook-f">Facebook</i></a>
-      <a href="https://www.twitter.com/redbeestudios" className="btn-red-social"><i className="fab fa-twitter">Twitter</i></a>
-      <a href="https://www.instagram.com/redbeestudios" className="btn-red-social"><i className="fab fa-instagram">Instagram</i></a>
-      <a href="https://www.youtube.com/redbeestudios" className="btn-red-social"><i className="fab fa-instagram">YouTube</i></a>
-  </footer>
-  </body>
-</App>
+    <main className="frontPage">
+      
+      <div className="header">
+        <img src={img} alt="isologo redbeeConf" className="logo"/>
+        <Button variant="outline-light" className="LogButton">Ingresar</Button>
+      </div>
+
+      <div className="content">
+        <div id='left'>
+          <h1>Sumate a la redbee conference</h1>
+          <p id='description'>Sé parte de una experiencia única dentro del mundo de TI, con más de +50 Speakers, +30 workshops en diferentes áreas, +100 charlas en vivo.</p>
+          <div className="buttons">
+            <Button variant="light">Inscribirme</Button>
+            <Button variant="light">Postulá tu charla aquí</Button> 
+          </div>
+      </div>
+        
+      </div>
+   
+    </main>
+    </App>
 }
 
 export default EndUserApp
