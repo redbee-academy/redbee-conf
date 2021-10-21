08ab988c23b283f9eb8c57277c9f0b582071007c
ximenafarinae <ximenafarinae@gmail.com>
2021-10-21 15:57:32 -0300

    fix: datepicker

--- web-app-conf/src/app/components/App.tsx
+++ web-app-conf/src/app/components/App.tsx
@@ -2,6 +2,8 @@ import { FunctionComponent } from 'react';
 import { AppConfiguration } from '../domain';
 import { AppConfigurationContext } from '../context';
 import { ConferenceComponent } from './Staff/Conferences/components/Conference/Conference';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns'
 
 const CONFIGURATION: AppConfiguration = {
   conferencesUrl: process.env.REACT_APP_MS_CONFERENCES_URL as string,
@@ -12,8 +14,11 @@ const CONFIGURATION: AppConfiguration = {
 export const App: FunctionComponent = ({ children }) => {
   return (
     <AppConfigurationContext.Provider value={CONFIGURATION}>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
       {children}
-      <ConferenceComponent></ConferenceComponent>
+      <ConferenceComponent></ConferenceComponent>  
+      </MuiPickersUtilsProvider>
+      
     </AppConfigurationContext.Provider>
   )
 }
