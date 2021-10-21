ea15899075716d5b742fb11c6afa7d5558a0c9b8
ximenafarinae <ximenafarinae@gmail.com>
2021-10-21 10:59:03 -0300

    fix: create conf form centered

--- web-app-conf/src/app/components/Staff/Conferences/components/Conference/Conference.tsx
+++ web-app-conf/src/app/components/Staff/Conferences/components/Conference/Conference.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent } from "react";
 import './conference.css'
-import { Button, Form, FormControl } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { Conference } from "../../domain/Conference";
 import { KeyboardDatePicker } from "@material-ui/pickers";
 
@@ -10,9 +10,8 @@ export interface ConferenceProps {
 }
 
 export const ConferenceComponent: FunctionComponent = () => (
-    
-    <Form className="conferenceMainForm" style={{ maxWidth: '1000px'}} >
-        <div>
+<div className="conferenceMainForm">
+    <Form style={{ maxWidth: '1000px' }} >
         <Form.Group className="mb-3" controlId="formBasicName">
             <Form.Label>Nombre de la proxima CONF</Form.Label>
             <Form.Control type="name" placeholder="Ingrese un nombre" /> {/* El placeholder no deberia estar ya que el nombre se genera automaticamente */}
@@ -25,7 +24,7 @@ export const ConferenceComponent: FunctionComponent = () => (
                   label="Fecha de finalización"
                   onChange={(date: date, value?: string | null) => }
                 />*/}
-        </Form.Group> 
+        </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicDescription">
             <Form.Label>Descripcion</Form.Label>
@@ -33,12 +32,12 @@ export const ConferenceComponent: FunctionComponent = () => (
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Visible"/>
+            <Form.Check type="checkbox" label="Visible" />
         </Form.Group>
         <Button variant="primary" type="submit">
             Submit
         </Button>
-        </div>
-        
-</Form>
+    </Form>
+</div>
+   
 )
