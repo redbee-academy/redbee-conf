e0eaf6f2110260968585bedf079e5db3c1819417
Joaco Campero <joaquin@redb.ee>
2021-10-23 01:14:16 -0300

    test: elimino tests de contexto

--- ms-speakers/src/test/java/io/redbee/product/conf/ms/speakers/SpeakersApplicationTests.java
+++ ms-speakers/src/test/java/io/redbee/product/conf/ms/speakers/SpeakersApplicationTests.java
@@ -1,13 +1,8 @@
 package io.redbee.product.conf.ms.speakers;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpeakersApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
