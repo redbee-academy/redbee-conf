fafa8bed841061001a369fb6ac42b09f171eb641
Marco <marco.scabbiolo@redb.ee>
2021-12-27 13:08:35 -0300

    fix(bo): fix internal routing in bo navbar

--- web-app-conf/src/app/components/Staff/Staff.tsx
+++ web-app-conf/src/app/components/Staff/Staff.tsx
@@ -10,22 +10,22 @@ import { STAFF_BASE_PATH, STAFF_CONFERENCES_PATH } from './const'
 
 const StaffApp: FunctionComponent = () => (
   <App>
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href={STAFF_BASE_PATH}>
-          <img src={logo} alt="logo" />
-        </Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link as={Link} to={STAFF_BASE_PATH}>
-            Home
-          </Nav.Link>
-          <Nav.Link as={Link} to={STAFF_CONFERENCES_PATH}>
-            Ediciones
-          </Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
     <Router>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href={STAFF_BASE_PATH}>
+            <img src={logo} alt="logo" />
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to={STAFF_BASE_PATH}>
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to={STAFF_CONFERENCES_PATH}>
+              Ediciones
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
       <Switch>
         <Route path={STAFF_BASE_PATH} exact>
           <StaffHome />
